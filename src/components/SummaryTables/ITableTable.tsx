36832c2b5b4120fb9e1f40f437b7707c6cdca874
Maaz966 <maazmukhtar@addo.ai>
2025-08-05 19:29:31 +0500

    chore: update spinner

--- src/components/SummaryTables/ITableTable.tsx
+++ src/components/SummaryTables/ITableTable.tsx
@@ -107,7 +107,7 @@ const ITableFromItems: React.FC<Props> = ({ pagination, items, headerRows, selec
                 </tr>
               ))}
             </thead>
-           {loading ? <Spinner animation="border" style={{ width: '300px', height: '300px', color: '#4F959D', display: 'block', marginTop: '60%', marginLeft: '100%' }} /> : <tbody>
+           {loading ? <Spinner animation="border" style={{ width: '50px', height: '50px', color: '#4F959D', display: 'block', marginTop: '100%', marginLeft: '150%' }} /> : <tbody>
               {items.map((item, index) => {
                 const leafNodes = selectedLeafNode.get(item.paper_id) || [];
                 return (
