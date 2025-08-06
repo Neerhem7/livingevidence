4198d5f805cc42c2b6182de82ec351e64c0c03c1
Maaz966 <maazmukhtar@addo.ai>
2025-08-06 17:13:39 +0500

    feat: Verify PMID link before redirecting user

--- src/components/Prisma/components/PaperCard.tsx
+++ src/components/Prisma/components/PaperCard.tsx
@@ -1,11 +1,32 @@
 import React from "react";
-import { Badge } from "react-bootstrap";
+import { Alert, Row, Spinner } from "react-bootstrap";
 import { PaperCardProps } from "../types";
 import "../prisma.css";
 import { getPaperLink, getPaperType } from "../../../utils/utils";
 
 const PaperCard: React.FC<PaperCardProps> = React.memo(
   ({ paper, onViewDetails }) => {
+    const [verifyingLink, setVerifyingLink] = React.useState<boolean>(false);
+    const [verificationFailed, setVerificationFailed] =
+      React.useState<boolean>(false);
+
+    const fetchPaperLink = () => {
+      if (paper && paper?.paper_id_type && paper?.paper_id) {
+        setVerifyingLink(true);
+        getPaperLink(paper?.paper_id_type, paper?.paper_id).then((link) => {
+          if (link) {
+            window.open(link, "_blank");
+          } else {
+            setVerificationFailed(!link);
+          }
+          setVerifyingLink(false);
+          setTimeout(() => {
+            setVerificationFailed(false);
+          }, 5000);
+        });
+      }
+    };
+
     const getYear = (dateStr: string) => {
       const yearFromDate = new Date(dateStr);
       if (
@@ -19,56 +40,82 @@ const PaperCard: React.FC<PaperCardProps> = React.memo(
     };
 
     return (
-      <div className="d-flex align-items-center border-bottom py-3 prisma-text col-12 col-md-12">
-        <div className="d-flex justify-content-center">
-          <small className="date-circle">{getYear(paper?.publish_date)}</small>
-        </div>
-
-        <div className="flex-grow-1 px-3">
-          <div className="text-truncate-2 paper-title">{paper?.title}</div>
-          <div className="paper-type d-flex align-items-start mt-2">
-            <span className="doi-pm">
-              <span>
-                {paper?.paper_id_type && getPaperType(paper?.paper_id_type)}:{" "}
-              </span>
-              {paper?.paper_id_type && (
-                <a
-                  href={
-                    paper?.paper_id &&
-                    getPaperLink(paper?.paper_id_type, paper?.paper_id)
-                  }
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {paper?.paper_id}
-                </a>
-              )}
-            </span>
-            {paper?.nct_number ? (
-              <span className="ms-2 nct-number">
-                <span>{"NCT"}: </span>
-                {
-                  <a
-                    href={`https://clinicaltrials.gov/study/${paper.nct_number}`}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    {paper?.nct_number}
-                  </a>
-                }
-              </span>
-            ) : null}
+      <>
+        <Row className="d-flex align-items-center justify-content-center">
+          <Alert
+            variant="danger"
+            role="alert"
+            className="d-flex justify-content-center text-center mt-2 mb-0 w-50"
+            show={verificationFailed}
+            onClose={() => setVerificationFailed(false)}
+            dismissible
+          >
+            No Paper Found with this PMID
+          </Alert>
+        </Row>
+        <div className="d-flex align-items-center border-bottom py-3 prisma-text col-12 col-md-12">
+          <div className="d-flex justify-content-center">
+            <small className="date-circle">
+              {getYear(paper?.publish_date)}
+            </small>
           </div>
-        </div>
 
-        <button
-          className="btn btn-primary ms-3"
-          onClick={() => onViewDetails(paper)}
-          aria-label="View paper details"
-        >
-          <i className="fa-solid fa-ellipsis" />
-        </button>
-      </div>
+          <div className="flex-grow-1 px-3">
+            <div className="text-truncate-2 paper-title">{paper?.title}</div>
+            <div className="paper-type d-flex align-items-start mt-2">
+              <span className="doi-pm">
+                <span>
+                  {paper?.paper_id_type && getPaperType(paper?.paper_id_type)}:{" "}
+                </span>
+                {paper?.paper_id_type && (
+                  <span
+                    style={{ cursor: "pointer", color: "#007bff" }}
+                    onClick={() => {
+                      !verifyingLink && fetchPaperLink();
+                    }}
+                  >
+                    {verifyingLink ? (
+                      <>
+                        Verifying Link...{" "}
+                        <Spinner animation="border" size="sm" />
+                      </>
+                    ) : (
+                      paper?.paper_id
+                    )}
+                  </span>
+                )}
+              </span>
+              {paper?.nct_number ? (
+                <span className="ms-2 nct-number">
+                  <span>{"NCT"}: </span>
+                  {
+                    <a
+                      href={`https://clinicaltrials.gov/study/${paper.nct_number}`}
+                      target="_blank"
+                      style={{
+                        outline: "none",
+                        textDecoration: "none",
+                        color: "#007bff",
+                      }}
+                      rel="noopener noreferrer"
+                    >
+                      {paper?.nct_number}
+                    </a>
+                  }
+                </span>
+              ) : null}
+            </div>
+          </div>
+
+          <button
+            className="btn btn-primary ms-3"
+            onClick={() => onViewDetails(paper)}
+            aria-label="View paper details"
+          >
+            <i className="fa-solid fa-ellipsis" />
+          </button>
+        </div>
+      </>
     );
   }
 );
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,7 +1,9 @@
 // utils.ts
 
+import axios from "axios";
+
 export interface CellData {
-  month: string,
+  month: string;
   total_papers: number | 0;
   count: number | 0;
 }
@@ -10,7 +12,10 @@ export type CalendarData = {
   [year: number]: (CellData | null)[];
 };
 
-export const generateCalendarData = (startDate: string | Date, endDate: Date): CalendarData => {
+export const generateCalendarData = (
+  startDate: string | Date,
+  endDate: Date
+): CalendarData => {
   const start = new Date(startDate);
   const end = new Date(endDate);
   const calendar: CalendarData = {};
@@ -30,7 +35,7 @@ export const generateCalendarData = (startDate: string | Date, endDate: Date): C
     const month = iter.getMonth(); // 0-indexed
 
     if (!calendar[year]) calendar[year] = Array(12).fill(null);
-    calendar[year][month] = { month: '', total_papers: 0, count: 0 };
+    calendar[year][month] = { month: "", total_papers: 0, count: 0 };
 
     iter.setMonth(iter.getMonth() + 1);
   }
@@ -38,12 +43,64 @@ export const generateCalendarData = (startDate: string | Date, endDate: Date): C
   return calendar;
 };
 
+const checkPubMedArticle = async (paperId: string) => {
+  const url = `https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&id=${paperId}&retmode=json`;
 
-export const getPaperLink = (paper_id_type: string, paper_id: string) => {
-  if (paper_id_type === "pubmed" && paper_id) {
-    return `https://pubmed.ncbi.nlm.nih.gov/${paper_id}`;
+  try {
+    const response = await axios.get(url);
+    const data = response.data;
+
+    if (data.result && data.result[paperId] && !data.result[paperId]?.error) {
+      return `https://pubmed.ncbi.nlm.nih.gov/${paperId}`;
+    }
+    return null;
+  } catch (error) {
+    console.error("Error accessing API:", error);
+    return null;
+  }
+};
+
+const verifyPaperLink = async (
+  paper_id_type: string,
+  paper_id: string
+): Promise<string | null> => {
+  let url = null;
+
+  if (paper_id_type === "PMID" && paper_id) {
+    url = `https://pubmed.ncbi.nlm.nih.gov/${paper_id}`;
   } else if (paper_id_type === "DOI" && paper_id) {
-    return `https://doi.org/${paper_id}`;
+    url = `https://doi.org/${paper_id}`;
+  }
+
+  if (url) {
+    try {
+      const response = await checkPubMedArticle(paper_id); // Perform a GET request to fetch the page content
+      if (response && response !== null && response?.length > 0) {
+        return response;
+      }
+    } catch (error) {
+      console.error(`Error checking URL: ${url}`, error);
+    }
+  }
+
+  return null;
+};
+
+export const getPaperLink = async (paper_id_type: string, paper_id: string) => {
+  if (paper_id_type === "pubmed" && paper_id) {
+    const paperLink = await verifyPaperLink("PMID", paper_id);
+    if (paperLink) {
+      return paperLink; // Redirect to the article
+    } else {
+      return "";
+    }
+  } else if (paper_id_type === "DOI" && paper_id) {
+    const paperLink = await verifyPaperLink("DOI", paper_id);
+    if (paperLink) {
+      return paperLink; // Redirect to the article
+    } else {
+      return "";
+    }
   }
 };
 
